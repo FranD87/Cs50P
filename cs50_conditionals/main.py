fb425cb81615798ed5ccae4de40e9553a22ba9e2
FranD87 <fdunne007@gmail.com>
2023-04-25 15:05:42 +0200

    Commit

---
+++ cs50_conditionals/main.py
@@ -0,0 +1,10 @@
+#Match & Case???
+name = input("What's your name? ")
+
+match name:
+    case "Harry" | "Hermione" | "Ron":
+        print("Gryffindor")
+    case "Draco":
+        print("Slytherin")
+    case _:
+        print("Who?")
